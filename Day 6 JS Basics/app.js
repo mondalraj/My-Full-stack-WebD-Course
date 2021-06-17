b08bba9c060f53146925e72d23de768908af9f89
Rajib Mondal <mondal021002rajib@gmail.com>
2021-06-17 21:30:26 +0530

    Day 6 Completed

---
+++ Day 6 JS Basics/app.js
@@ -0,0 +1,51 @@
+console.log("Hello World!");
+// datatypes -> var, const, let
+
+var name_1 = "Rajib";
+let name_2 = "Akash";
+var name_3 = "Nikita";
+const name_4 = "Gargi";
+name_3 = "Sneha";
+// syntax -> datatype variable_name
+
+console.log(name_3);
+
+// unary operator -> a++, ++a, --a, a--
+// binary operator -> a+b, a-b, a*b, a/b, a%b
+// ternary operator -> (condition) ? statement1 (true) : statement2 (false)
+
+console.log(3<5 ? 2 :4)
+
+// variables
+
+var number_1 = 5;
+var number_2 = "5";
+
+var number_3 = 3.14;
+var name = "Rajib";
+
+console.log(number_3);
+
+if(number_1 === number_2){
+    console.log("Yeah!");
+} else {
+    console.log("Nope");
+}
+
+// console.log(number);
+
+var is_loggedin_byfacebook = true;
+
+if(is_loggedin_byfacebook === true){
+    console.log("Welcome to the website");
+} else{
+    console.log("you'r kicked out of the website");
+}
+
+// int arr[4] = [2, 4, 5, 7]
+var numbers = [1, 3, 6, 8, 12, 3000];
+console.log(numbers);
+console.log(numbers[2]);
+
+var city = ["Delhi", true, 34, "Koltata", "Mumbai", "Bengaluru"]
+console.log(city[2]);
---
+++ Day 6 JS Basics/topics.md
@@ -0,0 +1,7 @@
+=> Today we will study about..  
+  
+JS datatypes -> var, let, const  
+Operators -> unary, binary, ternary  
+Operators -> + - * / = == === ! != %  
+Types of variables -> string, number, boolean, array, object  
+JS Conditionals -> if-else, for, while, do-while, switch-case, break  
